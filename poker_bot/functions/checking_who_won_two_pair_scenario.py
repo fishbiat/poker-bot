66644225c6a56c92ac527b2016b373dd0e28bb5f
landon <landon.fitzgerald777@gmail.com>
2023-07-23 20:58:41 -0400

    fix checking who won

--- poker_bot/functions/checking_who_won_two_pair_scenario.py
+++ poker_bot/functions/checking_who_won_two_pair_scenario.py
@@ -1,38 +1,26 @@
 def checking_who_won_two_pair_scenario(players, number_of_two_pair):
-
-    # When we start off, we haven't seen any pairs, so 
-    # the highest we have seen starts at 0
     highest_number_of_pairs = 0
-
-    # We haven't started looking at the hands yet, so this bucket of potential 
-    # winners is empty
     bucket_of_potential_winners = []
 
-    # We now loop through each player:
-    for k in range(1, len(players) +1):
+    for k in range(1, len(players) + 1):
+        pairs = number_of_two_pair[k]
 
-      # If the player has more pairs than the highest number of 
-      # pairs we saw previously, then that player is our new potential 
-      # winner, so empty the bucket of potential winners, and add
-      # this player to that bucket
-      if len(number_of_two_pair[k]) > highest_number_of_pairs:
-        # Since this player has a higher number of pairs than
-        # we have seen so far, the highest number of pairs is 
-        # set to the number of pairs that they have
-        highest_number_of_pairs = len(number_of_two_pair[k])
-        # Empty the bucket 
-        bucket_of_potential_winners = []
-        # Add ths player to the bucket of winners
-        bucket_of_potential_winners.append(k)
+        if len(pairs) > highest_number_of_pairs:
+            highest_number_of_pairs = len(pairs)
+            bucket_of_potential_winners = [k]
+        elif len(pairs) == highest_number_of_pairs:
+            max_pair_value = max(pairs) if pairs else -1
 
-      # In certain circumstances, there will be players who have
-      # the same amount of pairs, and in that case, both players 
-      # should be in the bucket of potential winners
-      elif len(number_of_two_pair[k]) == highest_number_of_pairs:
-        # add the player who tied the previos top player to the
-        # bucket of potential winners
-        bucket_of_potential_winners.append(k)
+            # Set a very low initial value for the current_winner_pair_value
+            current_winner_pair_value = float("-inf")
 
-    
-    # The function has to output something that we can use
+            # If there are potential winners, find the maximum pair value among them
+            if bucket_of_potential_winners:
+                current_winner_pair_value = max(number_of_two_pair[player][0] for player in bucket_of_potential_winners)
+
+            if max_pair_value > current_winner_pair_value:
+                bucket_of_potential_winners = [k]
+            elif max_pair_value == current_winner_pair_value:
+                bucket_of_potential_winners.append(k)
+
     return bucket_of_potential_winners
